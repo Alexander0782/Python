cb2dc5bdef7b0b928e77b4a3796ff6b2d2ed81d1
Alexander0782 <alexander.uribe.zambrano@alumnos.sip.cl>
2025-04-02 11:53:37 -0300

    actualizacion 01-04

---
+++ python/ControldeFlujo/and.py
@@ -0,0 +1,8 @@
+#Alexander Uribe 02/04/2025
+
+usuario = input("Ingresa tu nombre")
+hambre = int(input("¿Cuanta hambre tienes? (1-10)"))
+enojo = int(input("¿Cuanto enojo tienes? (1-10)"))
+
+if hambre > 4 and enojo >1:
+    print("enfadado por hambre")
---
+++ python/ControldeFlujo/or.py
@@ -0,0 +1,11 @@
+#Alexander Uribe 02/04/2025
+
+print("!Bienvenido a tu dia feliz¡")
+
+cafe = int(input("¿Cuantas tazas de cafe te haz tomado hoy? (0 o mas):"))
+te_burbujas = int(input("¿cuantas tazas de te de burbuja haz tomado hoy? (0 o mas):"))
+
+if cafe > 0 or te_burbujas > 0:
+    print("!Que bien¡ Has disfrutado de algo  rico hoy")
+else:
+    print("parece que no haz tomado nada especial hoy. !Animate¡")
